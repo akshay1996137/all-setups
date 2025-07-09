b4fdffb16ea6050c136b8e830c2cf9f384a1c7ab
akshay1996137 <83005391+akshay1996137@users.noreply.github.com>
2025-07-09 12:52:12 +0530

    Update nexus.sh

--- nexus.sh
+++ nexus.sh
@@ -1,8 +1,8 @@
 #create amazonlinux ec2 with t2.micro and 30 gb of ebs with port 8081 
 
-wget https://download.sonatype.com/nexus/3/nexus-unix-x86-64-3.79.0-09.tar.gz\
-tar -zxvf nexus-unix-x86-64-3.79.0-09.tar.gz
+wget https://download.sonatype.com/nexus/3/nexus-3.81.1-01-linux-x86_64.tar.gz
+tar -zxvf nexus-3.81.1-01-linux-x86_64.tar.gz
 yum install java-17-amazon-corretto -y
 sudo useradd nexus
-chown -R  nexus:nexus  nexus-3.79.0-09
-sudo sh nexus-3.79.0-09/bin/nexus start
+chown -R  nexus:nexus  nexus-3.81.1-01
+sudo sh nexus-3.81.1-01/bin/nexus start
